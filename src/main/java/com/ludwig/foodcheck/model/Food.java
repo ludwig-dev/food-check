c1455918789be87dce13148b513dc55e53862bc3
Ludwig <00ludwighahn@gmail.com>
2025-03-25 14:04:32 +0100

    Add Food entity

---
+++ src/main/java/com/ludwig/foodcheck/model/Food.java
@@ -0,0 +1,29 @@
+package com.ludwig.foodcheck.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livsmedel")
+@Getter
+@Setter
+public class Food {
+    @Id
+    private int nummer;
+
+    private String namn;
+
+    @Column(name = "vetenskapligt_namn")
+    private String vetenskapligtNamn;
+
+    @Column(name = "livsmedels_typ_id")
+    private int livsmedelsTypId;
+
+    @Column(name = "livsmedels_typ")
+    private String livsmedelsTyp;
+
+    private String version;
+    private String projekt;
+}
+
