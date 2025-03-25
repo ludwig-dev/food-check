347b00c4e022a4c3659fa6bcc56fa209cf7fc8c8
Ludwig <00ludwighahn@gmail.com>
2025-03-25 17:13:40 +0100

    Add repository for Recipe and RecipeIngredient

---
+++ src/main/java/com/ludwig/foodcheck/repository/RecipeIngredientRepository.java
@@ -0,0 +1,7 @@
+package com.ludwig.foodcheck.repository;
+
+import com.ludwig.foodcheck.model.RecipeIngredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+}
---
+++ src/main/java/com/ludwig/foodcheck/repository/RecipeRepository.java
@@ -0,0 +1,11 @@
+package com.ludwig.foodcheck.repository;
+
+import com.ludwig.foodcheck.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+
+}
