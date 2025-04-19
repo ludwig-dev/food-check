8b6ebccec01c2728caf9f2d303ab1a35b67cd228
Ludwig <00ludwighahn@gmail.com>
2025-04-19 14:13:01 +0200

    add UpdateRecipeNameDTO

---
+++ src/main/java/com/ludwig/foodcheck/recipe/dto/UpdateRecipeNameDTO.java
@@ -0,0 +1,12 @@
+package com.ludwig.foodcheck.recipe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateRecipeNameDTO {
+    private String name;
+}
